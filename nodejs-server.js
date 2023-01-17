d5e252ce754be09062e2bc091fd6ae2ee4840e63
Nicholas Boyle <n.boyle1714@gmail.com>
2023-01-17 11:49:18 -0500

    nodejs server

---
+++ nodejs-server.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const port = 3000;
+const host = 'localhost';
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello, World!');
+});
+
+server.listen(port, host, () => {
+    console.log(`Server running at ${host}:${port}`);
+});
